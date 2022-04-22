152f55a10623de0770dcdf9d2619a57760d9c26f
DongDongLim <99638155+DongDongLim@users.noreply.github.com>
2022-04-23 05:03:49 +0900

    작업 중
    Singleton 및 SoundMng 제작 중

---
+++ TeamPlaformer/Assets/Script/SoundMng.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMng : Singleton<SoundMng>
+{
+    enum AUDIO_TYPE
+    {
+        BGM,
+        SFX
+    }
+
+    public enum SFX_CLIP
+    {
+        SFX1,
+    }
+
+    public enum BGM_CLIP
+    {
+        BGM1
+    }
+
+    AudioSource[] m_audio;//오디오 소스를 담는 배열
+    [SerializeField]
+    AudioClip[] m_sfxClip;
+    [SerializeField]
+    AudioClip[] m_bgmClip;
+
+
+    public override void OnAwake()
+    {
+        AddInstance(this);
+
+        m_audio = new AudioSource[2];
+
+        // BGM
+        m_audio[(int)AUDIO_TYPE.BGM] = new AudioSource();
+
+        m_audio[(int)AUDIO_TYPE.BGM].playOnAwake = true;
+
+        m_audio[(int)AUDIO_TYPE.BGM].loop = true;
+
+    }
+}
---
+++ TeamPlaformer/Assets/Script/Util/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour
+{
+    private static T _instance;
+    public static T instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        OnAwake();
+    }
+
+    protected void AddInstance(T t)
+    {
+        _instance = t;
+    }
+
+    public abstract void OnAwake();
+
+}
