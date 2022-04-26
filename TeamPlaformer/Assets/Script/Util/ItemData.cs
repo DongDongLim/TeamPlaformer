8ae1dfcc14c00593841b5cf6646cb775b7325204
MoonSangDon <moon3225@naver.com>
2022-04-26 20:34:52 +0900

    MergeCommit
    MergeCommit

--- TeamPlaformer/Assets/Script/Util/ItemData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ItemData", menuName = "Data/ItemData")]
-public class ItemData : Data
-{
-
-}
